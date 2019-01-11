f5c268aeb442e5b3918d81a5dfd4663679c128a1
Saumya Trivedi <34698324+saumyatrivedi14@users.noreply.github.com>
2019-01-11 00:00:10 -0700

    Add files via upload

---
+++ Assignment2_CSpace/Inside_Triangle.m
@@ -0,0 +1,28 @@
+function [ inside ] = Inside_Triangle(P1, P2)
+inside = false;
+    
+    B =[P2(1,1) P2(1,2) 1;
+        P2(2,1) P2(2,2) 1;
+        P2(3,1) P2(3,2) 1];
+    
+    B1 =[P1(1,1) P1(1,2) 1;
+        P2(2,1) P2(2,2) 1;
+        P2(3,1) P2(3,2) 1];
+
+    B2 =[P2(1,1) P2(1,2) 1;
+        P1(1,1) P1(1,2) 1;
+        P2(3,1) P2(3,2) 1];
+
+    B3 =[P2(1,1) P2(1,2) 1;
+        P2(2,1) P2(2,2) 1;
+        P1(1,1) P1(1,2) 1];
+    
+    b = abs(det(B1)/2) + abs(det(B2)/2) + abs(det(B3)/2);
+    
+    if abs(det(B)/2) == b
+        inside = true;
+    end
+
+
+end
+
